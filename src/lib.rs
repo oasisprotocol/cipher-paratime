c966d7294d8548fddc4c439d8b2d40b4c5f4afed
Jernej Kos <jernej@kos.mx>
2021-08-03 10:34:19 +0200

    Add bridge module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 //! The Cipher ParaTime.
+use std::collections::{BTreeMap, BTreeSet};
+
 use oasis_runtime_sdk::{
-    self as sdk, modules,
+    self as sdk,
+    crypto::signature::PublicKey,
+    modules,
     types::token::{BaseUnits, Denomination},
     Version,
 };
@@ -22,6 +26,8 @@ impl sdk::Runtime for Runtime {
         modules::consensus_accounts::Module<modules::accounts::Module, modules::consensus::Module>,
         // Rewards.
         modules::rewards::Module<modules::accounts::Module>,
+        // Bridge.
+        bridge::Module<modules::accounts::Module>,
     );
 
     fn genesis_state() -> <Self::Modules as sdk::module::MigrationHandler>::Genesis {
@@ -70,6 +76,52 @@ impl sdk::Runtime for Runtime {
                     },
                     participation_threshold_numerator: 3,
                     participation_threshold_denominator: 4,
+                },
+            },
+            bridge::Genesis {
+                parameters: bridge::Parameters {
+                    witnesses: vec![
+                        PublicKey::Secp256k1("ApGjeGS0wK0iA7rcAtMwKpJv5By8nMFFWllkGd/RTOiN".into()), // Oasis Protocol Foundation
+                        PublicKey::Secp256k1("Ait1frjDUZSSjafXB1ahTLhV8H/rGlq7k94i6AyPMg0i".into()), // cdot
+                    ],
+                    threshold: 2,
+                    local_denominations: {
+                        let mut ld = BTreeSet::new();
+                        ld.insert(Denomination::NATIVE);
+                        ld
+                    },
+                    remote_denominations: {
+                        let mut rd = BTreeMap::new();
+                        rd.insert(
+                            "oETH".parse().unwrap(),
+                            "0000000000000000000000000000000000000000".into(),
+                        );
+                        rd.insert(
+                            "oUSDC".parse().unwrap(),
+                            "07865c6e87b9f70255377e024ace6630c1eaa37f".into(),
+                        );
+                        rd.insert(
+                            "oUSDT".parse().unwrap(),
+                            "471297b3cb1e9f71661e7a92ced4b4b2b6e5b4a2".into(),
+                        );
+                        rd.insert(
+                            "oDAI".parse().unwrap(),
+                            "829de23e270519c5978e350deb0ec5ef8cd6af28".into(),
+                        );
+                        rd.insert(
+                            "oLINK".parse().unwrap(),
+                            "d91633cd725f815085cce509dd6e0f09210a0400".into(),
+                        );
+                        rd.insert(
+                            "oWETH".parse().unwrap(),
+                            "e5a393784df17d9f9f679c013257e06e72c37c0a".into(),
+                        );
+                        rd.insert(
+                            "oWBTC".parse().unwrap(),
+                            "f76eb8c27df517c25b5d5f91498b5c6203bfe55a".into(),
+                        );
+                        rd
+                    },
                 },
             },
         )
