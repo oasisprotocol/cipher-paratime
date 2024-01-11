61e5d87542865668009a43ef203bb1d390205123
Jernej Kos <jernej@kos.mx>
2024-01-11 13:33:27 +0100

    runtime: Bump Testnet trust root

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -108,8 +108,8 @@ impl sdk::Runtime for Runtime {
         if is_testnet() {
             // Testnet.
             Some(TrustRoot {
-                height: 18280406,
-                hash: "4bf0975bf288928addd8e052cdff556033c507d057ad2183aefa8a240311d1a4".into(),
+                height: 19183313,
+                hash: "54d353e4cebe669384d682aea9d19e7bde9362008128944c6df8b4a594c1ab62".into(),
                 runtime_id: "0000000000000000000000000000000000000000000000000000000000000000"
                     .into(),
                 chain_context: "0b91b8e4e44b2003a7c5e23ddadb5e14ef5345c0ebcb3ddcae07fa2f244cab76"
