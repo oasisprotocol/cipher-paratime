6da7f43a58c4c0702ba51dacda808fc21c8a43cb
Jernej Kos <jernej@kos.mx>
2025-03-31 14:21:03 +0200

    runtime: Bump Oasis SDK to 0.13.0, add rofl-market module

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -32,7 +32,7 @@ const fn is_testnet() -> bool {
 const fn state_version() -> u32 {
     if is_testnet() {
         // Testnet.
-        11
+        12
     } else {
         // Mainnet.
         6
@@ -76,6 +76,70 @@ impl modules::rofl::Config for Config {
     const STAKE_APP_CREATE: BaseUnits = BaseUnits::new(100_000_000_000, Denomination::NATIVE);
 }
 
+impl module_rofl_market::Config for Config {
+    /// Module implementing the ROFL API.
+    type Rofl = modules::rofl::Module<Config>;
+
+    /// Gas cost of roflmarket.ProviderCreate call.
+    const GAS_COST_CALL_PROVIDER_CREATE: u64 = 6_000_000;
+    /// Gas cost of roflmarket.ProviderUpdate call.
+    const GAS_COST_CALL_PROVIDER_UPDATE: u64 = 6_000_000;
+    /// Gas cost of roflmarket.ProviderUpdateOffers call.
+    const GAS_COST_CALL_PROVIDER_UPDATE_OFFERS_BASE: u64 = 6_000_000;
+    /// Gas cost of each added offer in roflmarket.ProviderUpdateOffers call.
+    const GAS_COST_CALL_PROVIDER_UPDATE_OFFERS_ADD: u64 = 600_000;
+    /// Gas cost of each removed offer in roflmarket.ProviderUpdateOffers call.
+    const GAS_COST_CALL_PROVIDER_UPDATE_OFFERS_RM: u64 = 60_000;
+    /// Gas cost of roflmarket.ProviderRemove call.
+    const GAS_COST_CALL_PROVIDER_REMOVE: u64 = 6_000_000;
+    /// Gas cost of roflmarket.InstanceCreate call.
+    const GAS_COST_CALL_INSTANCE_CREATE: u64 = 6_000_000;
+    /// Gas cost of roflmarket.InstanceAccept call.
+    const GAS_COST_CALL_INSTANCE_ACCEPT_BASE: u64 = 600_000;
+    /// Gas cost of each accepted instance in roflmarket.InstanceAccept call.
+    const GAS_COST_CALL_INSTANCE_ACCEPT_INSTANCE: u64 = 600_000;
+    /// Gas cost of roflmarket.InstanceTopUp call.
+    const GAS_COST_CALL_INSTANCE_TOPUP: u64 = 600_000;
+    /// Gas cost of roflmarket.InstanceUpdateMetadata call.
+    const GAS_COST_CALL_INSTANCE_UPDATE_METADATA: u64 = 6_000_000;
+    /// Gas cost of roflmarket.InstanceCancel call.
+    const GAS_COST_CALL_INSTANCE_CANCEL: u64 = 600_000;
+    /// Gas cost of roflmarket.InstanceRemove call.
+    const GAS_COST_CALL_INSTANCE_REMOVE: u64 = 600_000;
+    /// Gas cost of roflmarket.InstanceExecuteCmds call.
+    const GAS_COST_CALL_INSTANCE_EXECUTE_CMDS_BASE: u64 = 600_000;
+    /// Gas cost of each command in roflmarket.InstanceExecuteCmds call.
+    const GAS_COST_CALL_INSTANCE_EXECUTE_CMDS_CMD: u64 = 600_000;
+    /// Gas cost of roflmarket.InstanceCompleteCmds call.
+    const GAS_COST_CALL_INSTANCE_COMPLETE_CMDS_BASE: u64 = 600_000;
+    /// Gas cost of each command in roflmarket.InstanceCompleteCmds call.
+    const GAS_COST_CALL_INSTANCE_COMPLETE_CMDS_CMD: u64 = 600_000;
+    /// Gas cost of roflmarket.InstanceClaimPayment call.
+    const GAS_COST_CALL_INSTANCE_CLAIM_PAYMENT_BASE: u64 = 600_000;
+    /// Gas cost of each instance in roflmarket.InstanceClaimPayment call.
+    const GAS_COST_CALL_INSTANCE_CLAIM_PAYMENT_INST: u64 = 600_000;
+
+    /// Maximum time for a provider to accept an instance. If not accepted within this window, the
+    /// instance may be cancelled and will be refunded.
+    const MAX_INSTANCE_ACCEPT_TIME_SECONDS: u64 = 300;
+    /// Maximum number of offers a provider can have.
+    const MAX_PROVIDER_OFFERS: u64 = 64;
+    /// Maximum number of queued instance commands.
+    const MAX_QUEUED_INSTANCE_COMMANDS: u64 = 8;
+    /// Maximum size of an instance command.
+    const MAX_INSTANCE_COMMAND_SIZE: usize = 16 * 1024;
+
+    /// Maximum number of metadata key-value pairs.
+    const MAX_METADATA_PAIRS: usize = 64;
+    /// Maximum metadata key size.
+    const MAX_METADATA_KEY_SIZE: usize = 1024;
+    /// Maximum metadata value size.
+    const MAX_METADATA_VALUE_SIZE: usize = 16 * 1024;
+
+    /// Amount of stake required for maintaining a provider (100 ROSE/TEST).
+    const STAKE_PROVIDER_CREATE: BaseUnits = BaseUnits::new(100_000_000_000, Denomination::NATIVE);
+}
+
 /// The Cipher ParaTime.
 pub struct Runtime;
 
@@ -114,6 +178,8 @@ impl sdk::Runtime for Runtime {
         modules::rofl::Module<Config>,
         // Contracts.
         module_contracts::Module<Config>,
+        // ROFL market.
+        module_rofl_market::Module<Config>,
     );
 
     #[cfg(feature = "debug-mock-sgx")]
@@ -249,6 +315,7 @@ impl sdk::Runtime for Runtime {
                     ..Default::default()
                 },
             },
+            module_rofl_market::Genesis::default(),
         )
     }
 
@@ -272,5 +339,7 @@ impl sdk::Runtime for Runtime {
         modules::rofl::Module::<Config>::set_params(genesis.5.parameters);
         // Contracts.
         module_contracts::Module::<Config>::set_params(genesis.6.parameters);
+        // ROFL market.
+        module_rofl_market::Module::<Config>::set_params(genesis.7.parameters);
     }
 }
